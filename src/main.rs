4188dc5be7b31ccf79b52612ed24210c6c4083cf
Francis Tseng <accounts@frnsys.com>
2026-01-20 19:41:58 +0100

    feat: basic selection

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use egui::{
     Color32, FontId, Id, LayerId, Order, Painter, PointerButton, Pos2, Rangef, Rect, Response,
-    Sense, UiBuilder, Vec2, emath::TSTransform,
+    Sense, Stroke, StrokeKind, UiBuilder, Vec2, ahash::HashSet, emath::TSTransform,
 };
 
 struct App {
     transform: TSTransform,
     objects: Vec<Object>,
+    selected: HashSet<usize>,
 }
 impl App {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         Self {
             transform: TSTransform::default(),
+            selected: HashSet::default(),
             objects: vec![
                 Object {
                     transform: TSTransform::default(),
@@ -46,17 +48,11 @@ impl App {
 }
 
 enum InteractMode {
-    Panning,
-    Zooming,
+    Moving,
     Typing,
     Selecting,
     Drawing,
-}
-
-// Operating on a selection
-enum EditMode {
-    Moving,
-    Scaling,
+    Placing,
 }
 
 struct Object {
@@ -77,16 +73,19 @@ enum ObjectKind {
     },
 }
 impl ObjectKind {
-    fn paint(&self, painter: &Painter, pos: Pos2) {
+    fn paint(&self, painter: &Painter) -> Rect {
         match self {
             ObjectKind::Rect { size, color } => {
-                painter.rect_filled(Rect::from_min_size(pos, *size), 0., *color);
+                let rect = Rect::from_min_size(Pos2::ZERO, *size);
+                painter.rect_filled(rect, 0., *color);
+                rect
             }
             ObjectKind::Text { text, width, color } => {
                 const FONT: FontId = FontId::proportional(12.);
                 // PERF: Could probably cache this and only update when width or text changes.
                 let galley = painter.layout(text.to_string(), FONT, *color, *width);
-                painter.galley(pos, galley, *color);
+                painter.galley(Pos2::ZERO, galley.clone(), *color);
+                galley.rect
             }
         }
     }
@@ -100,7 +99,15 @@ impl eframe::App for App {
             update_transform(ui, &mut self.transform, &mut resp);
         });
 
-        for (i, obj) in self.objects.iter().enumerate() {
+        let clicked = ctx.input(|inp| inp.pointer.primary_clicked());
+        let interact_pos = if clicked {
+            ctx.input(|inp| inp.pointer.interact_pos())
+        } else {
+            None
+        };
+        let shift_pressed = ctx.input(|inp| inp.modifiers.shift_only());
+        let mut selection_rect: Option<Rect> = None;
+        for (i, obj) in self.objects.iter_mut().enumerate() {
             let mut trans = obj.transform;
             trans.scaling *= self.transform.scaling;
             trans.translation =
@@ -109,7 +116,40 @@ impl eframe::App for App {
             let layer = LayerId::new(Order::Middle, Id::new(i));
             ctx.set_transform_layer(layer, trans);
             let painter = ctx.layer_painter(layer);
-            obj.data.paint(&painter, Pos2::ZERO);
+            let rect = obj.data.paint(&painter);
+
+            if let Some(pos) = interact_pos
+                && rect.contains(trans.inverse().mul_pos(pos))
+            {
+                if self.selected.contains(&i) {
+                    if !shift_pressed {
+                        self.selected.clear();
+                        selection_rect = None;
+                    }
+                    self.selected.remove(&i);
+                } else {
+                    if !shift_pressed {
+                        self.selected.clear();
+                        selection_rect = None;
+                    }
+                    self.selected.insert(i);
+                }
+            }
+
+            if self.selected.contains(&i) {
+                let rect =
+                    Rect::from_min_size(trans.translation.to_pos2(), rect.size() * trans.scaling);
+                selection_rect = match selection_rect {
+                    Some(base) => Some(base.union(rect)),
+                    None => Some(rect),
+                };
+            }
+        }
+
+        if let Some(rect) = selection_rect {
+            let layer = LayerId::new(Order::Foreground, Id::new("selection"));
+            let painter = ctx.layer_painter(layer);
+            painter.rect_stroke(rect, 0., Stroke::new(2., Color32::RED), StrokeKind::Outside);
         }
     }
 }
